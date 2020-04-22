e91f0dd4d8d613bd175d6bb109432a5c233a06f1
HYUK LEE <ruldes76@gmail.com>
2020-04-22 10:40:50 +0900

    commit

--- hello.pl
+++ hello.pl
@@ -1,1 +1,17 @@
+use strict;
+use warnings;
+
 print "Hello hyuk lee \n";
+
+my $cnt = 0;
+
+while ($cnt <= 5) {
+    #redo演算子が実行された場合,この位置に進む
+    $cnt++;
+
+    if ($cnt == 6) {
+        redo;
+    }
+
+    print "$cnt\n";
+}
