25b3d781a5ede602a94a12f9397b1e8a3aabe378
HYUK LEE <ruldes76@gmail.com>
2020-04-23 13:16:02 +0900

    commit

--- HelloWorld.pl
+++ HelloWorld.pl
@@ -3,15 +3,11 @@ use warnings;
 
 print "Hello world perl !!!! \n";
 
-my $cnt = 0;
 
-while ($cnt <= 5) {
-    #redo演算子が実行された場合,この位置に進む
-    $cnt++;
 
-    if ($cnt == 6) {
-        redo;
-    }
-
-    print "$cnt\n";
+sub is_int {
+  my $num = shift;
+  
+  # 値が正の整数でない場合に例外を発生させる
+  die "\"$num\" must be number" unless $num =~ /^\d+$/;
 }
