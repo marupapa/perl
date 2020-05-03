a0d16d62d6b313c0dc7f172392e765f369bdc1c8
HYUK LEE <ruldes76@gmail.com>
2020-05-03 21:15:50 +0900

    commit

--- ThreadTes.pl
+++ ThreadTes.pl
@@ -1,34 +1,34 @@
-#!/opt/local/bin/perl
+# #!/opt/local/bin/perl
  
-use strict;
-use warnings;
-use utf8;
-use threads;
-use Time::HiRes;
+# use strict;
+# use warnings;
+# use utf8;
+# use threads;
+# use Time::HiRes;
  
-print "Content-type: text/html; charset=utf-8\n\n";
+# print "Content-type: text/html; charset=utf-8\n\n";
  
-my $threads = sub
-{
-    my $name = shift;
-    for ('A' .. 'E')
-    {
-        print +("${name} - ${_}"), "\n";
-        Time::HiRes::sleep(0.1);
-        threads->yield();
-    }
-};
+# my $threads = sub
+# {
+#     my $name = shift;
+#     for ('A' .. 'E')
+#     {
+#         print +("${name} - ${_}"), "\n";
+#         Time::HiRes::sleep(0.1);
+#         threads->yield();
+#     }
+# };
  
-my $first  = threads->new(\&$threads, "1");
-my $second = threads->new(\&$threads, "2");
-my $third  = threads->new(\&$threads, "3");
-my $fourth = threads->new(\&$threads, "4");
-my $fifth  = threads->new(\&$threads, "5");
+# my $first  = threads->new(\&$threads, "1");
+# my $second = threads->new(\&$threads, "2");
+# my $third  = threads->new(\&$threads, "3");
+# my $fourth = threads->new(\&$threads, "4");
+# my $fifth  = threads->new(\&$threads, "5");
  
-$first->join;
-$second->join;
-$third->join;
-$fourth->join;
-$fifth->join;
+# $first->join;
+# $second->join;
+# $third->join;
+# $fourth->join;
+# $fifth->join;
  
-exit;
+# exit;
